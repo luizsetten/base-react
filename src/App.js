d5ae0af6e8d6d7be31c79a3c4b581670efd40854
Luiz Setten <luizgustavosetten@hotmail.com>
2020-03-22 19:34:25 -0300

    Commit innicial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import history from './services/history';
+import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
+import Routes from './routes';
+
+function App() {
+  return (
+    <>
+      <Router history={history}>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </>
+  );
+}
+
+export default App;
